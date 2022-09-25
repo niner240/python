12458c77b446c371903092e902b5bc9a04548ff4
niner240 <113818131+niner240@users.noreply.github.com>
2022-09-25 12:58:33 +0700

    ..

---
+++ main.py
@@ -0,0 +1,23 @@
+# print("Введи стоимость одного пирожка")
+# rub = int(input("Рублей"))
+# cop = int(input("Копейки"))
+# price_pie =rub * 100 + cop
+# count = int(input("Сколько пирожков"))
+# need_to_pay = count * price_pie
+# print(f"С вас {need_to_pay // 100}) рублей и {need_to_pay % 100} копеек")
+# money = int(input("Даю денег"))
+# money = sdacha_cop * 100
+# print(f"Cдача{money_cop - need_to_pay}")x =
+# x = int(input("Число"))
+# if x % 2 == 1:
+#     x = x +1
+# else:
+#     x = x + 2
+#     print(x)
+#d = int(input("Диаметр окружности:"))
+#print("Длина =", 3.14 *d)
+n = float(input("секунды с начала дня:"))
+h = n // 3600
+m = n // 60
+s = n % 60
+print(f"Прошло {h} часов и {m} минут")
