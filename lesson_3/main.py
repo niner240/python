f2b3f821a04ed4fc8fb1eb779bf20be082bb548d
wertus23003 <wertus23003@gmail.com>
2022-10-02 13:09:05 +0700

    ...

---
+++ lesson_3/main.py
@@ -0,0 +1,65 @@
+#
+# # # print("Введи стоимость одного пирожка")
+# # # rub = int(input("Рублей"))
+# # # cop = int(input("Копейки"))
+# # # price_pie =rub * 100 + cop
+# # # count = int(input("Сколько пирожков"))
+# # # need_to_pay = count * price_pie
+# # # print(f"С вас {need_to_pay // 100}) рублей и {need_to_pay % 100} копеек")
+# # # money = int(input("Даю денег"))
+# # # money = sdacha_cop * 100
+# # # print(f"Cдача{money_cop - need_to_pay}")x =
+# # # x = int(input("Число"))
+# # # if x % 2 == 1:
+# # #     x = x +1
+# # # else:
+# # #     x = x + 2
+# # #     print(x)
+# # #d = int(input("Диаметр окружности:"))
+# # #print("Длина =", 3.14 *d)
+# # n = float(input("секунды с начала дня:"))
+# # h = n // 3600
+# # m = n // 60
+# # s = n % 60
+# # print(f"Прошло {h} часов и {m} минут")
+# # print("Привет")
+# # lst = ["А","Б","В","Г","Д"]
+# # print(lst[0],lst[1],lst[2],lst[3],lst[4],)
+# # print(lst[0:5])
+# # print(lst[0:5:2])
+# # x = "Антон"
+# # print(x[0:3])
+# # x = input("Введи слово")
+# # temp = x[-1]
+# # print(temp)
+# # print (x.index(temp) + 1)
+# #
+# # print(len(x))
+# #   = "C:\Python3\python.exe"
+# #
+# # print("Имя файла", x[11:22])
+# # print("Имя файла", x[-10])
+# # print("Расширение",x[ -3:])
+# # print("Имя каталога:", x[3:10])
+# # print("Полный путь к каталогу:", x[0:10])
+# #
+# # x1 = x.split("/")
+# # print(x1)
+# # print("Имя файла",
+#
+# print(x.rjust(50))
+# # chapter_2= input("Глава 1:")
+# # page_=input("Страница: ")
+# # chapter_1 = input("Глава 1:")
+# # page_=input("Страница: ")
+# # chapter_1 = input("Глава 1:")
+# # page_=input("Страница: ")
+# #
+# # print(chapter_1 + page_1.rjust(30))
+#
+# text = "12'345'678"
+# print(text.split("'")
+# print(temp)
+# temp2=temp[0] + temp[1] + temp[2])
+# print(temp2)
+# number = int(temp2)
